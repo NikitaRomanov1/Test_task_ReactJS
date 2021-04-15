81ad96bce4674eade308d0ef37aee3ed4381fc3f
NikitaRomanov1 <nikita038rus@gmail.com>
2021-04-15 15:50:39 +0300

    Updates

---
+++ precache-manifest.aa7d7a48d80d4aab2094530cbb7b8c4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d3dce3ccfec676d0497b08e79ca3823",
+    "url": "/Test_task_ReactJS/index.html"
+  },
+  {
+    "revision": "7f162b2fdc22c7c6f841",
+    "url": "/Test_task_ReactJS/static/css/main.c186f866.chunk.css"
+  },
+  {
+    "revision": "b34224dc48e803431eb3",
+    "url": "/Test_task_ReactJS/static/js/2.3a4d06dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Test_task_ReactJS/static/js/2.3a4d06dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f162b2fdc22c7c6f841",
+    "url": "/Test_task_ReactJS/static/js/main.52836651.chunk.js"
+  },
+  {
+    "revision": "9e7cce2247cd75974d9c",
+    "url": "/Test_task_ReactJS/static/js/runtime-main.34d0f35e.js"
+  }
+]);
